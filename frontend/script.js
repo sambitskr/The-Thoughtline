edecde8624cbb674c9dd0f7ab6e86e488dd876e5
sambit-innofied <sambit.sarkar@innofied.com>
2025-11-20 15:13:09 +0530

    building the carousel for blog post

--- frontend/script.js
+++ frontend/script.js
@@ -3,4 +3,13 @@ $(document).ready(function () {
     $(".nav").toggleClass("showing");
     $(".nav ul").toggleClass("showing");
   });
+
+  $(".post-wrapper").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+  });
 });
